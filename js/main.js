5f99d81898604da24aeb9916bc066a1da16a0a1f
xacer <chacecaven@outlook.com>
2020-11-18 20:40:12 -0500

    Test fetch with csv

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-catchImage("/js/calvin-surprised.png", "target-img");
+catchImage("/img/calvin-surprised.png", "target-img");
 
 async function catchImage (pathToImage, imgId) {
     console.log("Catching image: " + pathToImage);
@@ -8,4 +8,11 @@ async function catchImage (pathToImage, imgId) {
     console.log(blob);
     document.getElementById(imgId).src = URL.createObjectURL(blob);
     console.log("Finished operation");
+}
+
+
+async function catchCSV (pathToCSV) {
+    const response = await fetch("/csv/test.csv");
+    const data = await response.text();
+    console.log(data);
 }
