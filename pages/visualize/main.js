c9fa7115b9e9f844fe863eed74d8e7e118db7230
xacer <chacecaven@outlook.com>
2021-09-15 17:48:12 -0400

    Added visualize page

---
+++ pages/visualize/main.js
@@ -0,0 +1,140 @@
+
+import * as vec3 from "https://cdn.jsdelivr.net/gh/ccaven/xacer3d/src/gl-matrix/vec3.js";
+import * as mat4 from "https://cdn.jsdelivr.net/gh/ccaven/xacer3d/src/gl-matrix/mat4.js";
+
+let width = 400;
+let height = 400;
+
+/**
+ * @type {HTMLCanvasElement}
+ */
+const canvas = document.getElementById("output");
+
+canvas.width = width;
+canvas.height = height;
+
+const gl = canvas.getContext("webgl");
+
+/**
+ * @type {HTMLInputElement}
+ */
+const fileInput = document.getElementById("data-input");
+
+/**
+ * @type {HTMLInputElement[]}
+ */
+const resolutions = [
+    document.getElementById("select-tiny"),
+    document.getElementById("select-medium"),
+    document.getElementById("select-large")
+];
+
+/**
+ * @type {HTMLButtonElement}
+ */
+const generateButton = document.getElementById("generate-button");
+
+/**
+ * @type {HTMLButtonElement[]}
+ */
+const saveButtons = [
+    document.getElementById("save-png"),
+    document.getElementById("save-jpeg"),
+    document.getElementById("save-ico"),
+];
+
+const resolutionSizes = [
+    [ 20, 20 ],
+    [ 50, 50 ],
+    [ 100, 100]
+];
+
+(async () => {
+    if (!gl) {
+        alert("Your browser does not support WebGL!\nPlease update your browser, use a different one, or try a different computer.");
+        return;
+    }
+
+    gl.clearColor(1, 0.9, 0.9, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    const vertexSource = await fetch("./shaders/vertex.glsl").then(r => r.text());
+    const fragmentSource = await fetch("./shaders/fragment.glsl").then(r => r.text());
+
+    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, vertexSource);
+    gl.compileShader(vertexShader);
+
+    const vertexCompileSuccess = gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS);
+
+    if (!vertexCompileSuccess) {
+        console.error(gl.getShaderInfoLog(vertexShader));
+        return;
+    }
+
+    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, fragmentSource);
+    gl.compileShader(fragmentShader);
+
+    const fragmentCompileSuccess = gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS);
+
+    if (!fragmentCompileSuccess) {
+        console.error(gl.getShaderInfoLog(fragmentShader));
+        return;
+    }
+
+    const program = gl.createProgram();
+
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+
+    gl.linkProgram(program);
+
+    gl.useProgram(program);
+
+    gl.viewport(0, 0, width, height);
+
+    const positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    const positionData = [-1, -1, +1, -1, -1, +1, -1, +1, +1, -1, +1, +1];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positionData), gl.STATIC_DRAW);
+
+    const locations = {
+        "a_position": gl.getAttribLocation(program, "a_position"),
+        "u_resolution": gl.getUniformLocation(program, "u_resolution"),
+        "u_camera": gl.getUniformLocation(program, "u_camera"),
+        "u_view": gl.getUniformLocation(program, "u_view"),
+        "u_time": gl.getUniformLocation(program, "u_time"),
+        "u_anim": gl.getUniformLocation(program, "u_anim")
+    };
+
+    gl.enableVertexAttribArray(locations.a_position);
+    gl.vertexAttribPointer(locations.a_position, 2, gl.FLOAT, false, 0, 0);
+
+    gl.uniform2f(locations.u_resolution, width, height);
+
+    const camera = {
+        position: vec3.create(),
+        viewMatrix: mat4.identity(mat4.create())
+    };
+
+    function orbitCamera (r) {
+        camera.position[0] = Math.cos(r) * 2.5;
+        camera.position[2] = Math.sin(r) * 2.5;
+
+        mat4.fromRotation(camera.viewMatrix, Math.PI / 2 + r, [0, 1, 0]);
+    }
+
+    orbitCamera(Math.PI / 4);
+
+    generateButton.onclick = (e) => {
+
+        gl.uniform3fv(locations.u_camera, camera.position);
+        gl.uniformMatrix4fv(locations.u_view, false, camera.viewMatrix);
+
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        gl.drawArrays(gl.TRIANGLES, 0, 6);
+    };
+
+}) ();
---
+++ pages/visualize/shaders/fragment.glsl
@@ -0,0 +1,119 @@
+precision highp float;
+
+uniform vec2 u_resolution;
+
+uniform vec3 u_camera;
+uniform mat4 u_view;
+
+
+uniform sampler2D u_skybox;
+
+#define PI 3.1415926353
+#define ITERATIONS 1000
+#define EPSILON 0.002
+
+vec4 qadd (vec4 a, float s) {
+    return vec4(a.x + s, a.y, a.z, a.w);
+}
+vec4 qmul(vec4 q1, vec4 q2) {
+    return vec4(q1.x * q2.x - dot(q1.yzw, q1.yzw),
+        q1.x * q2.yzw + q2.x * q1.yzw + cross(q1.yzw, q2.yzw));
+}
+vec4 qconj (vec4 a) {
+    return vec4(a.x, -a.yzw);
+}
+vec4 qinv (vec4 a) {
+    return qconj(a) / dot(a, a);
+}
+vec4 qdiv (vec4 a, vec4 b) {
+    return qmul(a, qinv(b));
+}
+vec4 qexp (vec4 a) {
+    float l = length(a.yzw);
+    return exp(a.x) * vec4(cos(l), sin(l) / l * a.yzw);
+}
+vec4 qln (vec4 a) {
+    float l2 = length(a);
+    return vec4(log(l2), a.yzw / length(a.yzw) * acos(a.x / l2));
+}
+vec4 qpow (vec4 a, float p) {
+    return qexp(p*qln(a));
+}
+vec4 qsqr (vec4 a) {
+    return qmul(a, a);
+}
+vec4 qsqrt (vec4 a) {
+    return qpow(a, 0.5);
+}
+
+vec4 f (vec4 z, vec4 c) {
+    return qpow(z, 4.0) - 1.0 * qsqr(z) - qmul(z, c) - c;
+}
+vec4 df (vec4 z, vec4 c) {
+    vec4 e = vec4(0.01, 0.0, 0.0, 0.0);
+    return qdiv(f(z, c) - f(z + e, c), e);
+}
+
+float de (vec3 p) {
+    const vec4 c = vec4(0.1, 0.5, -0.2, 0.0);
+    vec4 z = vec4(p, 0.0);
+    vec4 dz = vec4(1.0, 0.0, 0.0, 0.0);
+
+    for (int i = 0; i < 20; i ++) {
+        if (dot(z, z) > 4.0) break;
+        dz = qmul(dz, df(z, c));
+        z = f(z, c);
+    }
+    float h = 0.0125 * log(dot(z, z)) * sqrt(dot(z, z) / dot(dz, dz));
+    return h - 0.001;
+}
+
+vec3 de_normal (vec3 p) {
+    const float h = 0.01;
+    const vec2 k = vec2(1,-1);
+    return normalize(
+        k.xyy * de( p + k.xyy*h ) +
+        k.yyx * de( p + k.yyx*h ) +
+        k.yxy * de( p + k.yxy*h ) +
+        k.xxx * de( p + k.xxx*h ) );
+}
+
+float raytrace (vec3 origin, vec3 direction, float minDistance, float maxDistance) {
+    float totalDistance = minDistance;
+    for (int i = 0; i < ITERATIONS; i ++) {
+        float localDistance = de(origin + direction * totalDistance);
+        totalDistance += localDistance;
+        if (localDistance < EPSILON) {
+            return totalDistance;
+        }
+        if (totalDistance > maxDistance) {
+            return -1.0;
+        }
+    }
+    return -1.0;
+}
+
+void main () {
+    vec2 screen_uv = (gl_FragCoord.xy / u_resolution) * 2.0 - 1.0;
+
+    vec2 camera_uv = screen_uv * u_resolution.xy / u_resolution.yy;
+
+    vec3 camera_position = u_camera;
+    vec3 camera_ray = normalize(vec3(camera_uv, 1.0)) * mat3(u_view);
+
+    float d = raytrace(camera_position, camera_ray, 0.0, 100.0);
+
+    if (d < 0.0) {
+        gl_FragColor = vec4(0, 0, 0, 0);
+        return;
+    }
+
+    vec3 hit = camera_position + camera_ray * d;
+    vec3 nor = de_normal(hit);
+
+    vec3 ld = normalize(vec3(1, 1, -1));
+
+    float diffuse = dot(nor, ld) * 0.5 + 0.5;
+
+    gl_FragColor = vec4(vec3(1, 1, 1) * diffuse, 1);
+}
