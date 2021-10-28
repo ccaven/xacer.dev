5030f5b177d7f6fa2e6a3220bc5604ecd5d885be
xacer <chacecaven@outlook.com>
2021-10-28 14:13:32 -0400

    Hax 2

--- misc/hax.js
+++ misc/hax.js
@@ -1,2 +1,16 @@
 
 console.log("Hax run");
+
+const canvas = document.createElement("canvas");
+
+canvas.style.width = "400px";
+canvas.style.height = "400px";
+
+canvas.width = 400;
+canvas.height = 400;
+
+document.body.appendChild(canvas);
+
+const ctx = canvas.getContext('2d');
+
+ctx.fillRect(10, 10, 100, 100);
